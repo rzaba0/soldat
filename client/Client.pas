2e1c98015e93978aca85b914f1d4f0266a5bee94
Jan Różański <20687913+rzaba0@users.noreply.github.com>
2020-10-04 02:30:40 +0200

    Bugfix. Some error messages (failed join request for example) are only shown for a split second

--- client/Client.pas
+++ client/Client.pas
@@ -403,7 +403,7 @@ begin
   GOALTICKS := DEFAULT_GOALTICKS;
 
   // Reset network state and show the status string (if any)
-  //ShouldRenderFrames := False;
+  ShouldRenderFrames := False;
   //NetEncActive := False;
   ResetSyncCvars;
 
